b63069aeaf6230c016b7f07af509e3ce2c6c7061
unknown <longnd_58@vnu.edu.vn>
2015-03-04 22:17:09 +0700

    t4

---
+++ app/controllers/Products.java
@@ -0,0 +1,21 @@
+package controllers;
+import play.mvc.*;
+import views.html.*;
+import models.Product.*;
+public class Products extends Controller {
+	public static Result list() {
+		return ok();
+	}
+	
+		public static Result newProduct() {
+		return ok();
+	}
+		public static Result details( String ean) {
+		return ok();
+	}
+		public static Result save() {
+		return ok();
+	}
+	
+	
+}
