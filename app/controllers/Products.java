38829eee8644a5485b48fcb310de4682e67cf2af
longnd_58 <longnd_58@vnu.edu.vn>
2015-03-05 21:36:39 +0700

    05/3 added Create functionality to Products

--- app/controllers/Products.java
+++ app/controllers/Products.java
@@ -1,10 +1,11 @@
 package controllers;
 import play.mvc.*;
+import play.data.*;
 import views.html.*;
  import java.util.ArrayList;
 import java.util.List;
 import models.Product;
-import views.html.products.list;
+import views.html.products.*;
 // import thư mục liên quan
 // import play.mvc.*;
 //import views.html.*;
@@ -13,15 +14,15 @@ import views.html.products.list;
 //import models.Product;
 //  import views.html.products.list;
 public class Products extends Controller {
-
+private static final Form<Product> productForm = Form.form(Product.class);
 	 public static Result list() {
     List<Product> products = Product.findAll();
     return ok(list.render(products));
   }
 	
-		public static Result newProduct() {
-		return ok();
-	}
+	public static Result newProduct(){
+  return ok(details.render(productForm));
+}
 		public static Result details( String ean) {
 		return ok();
 	}
